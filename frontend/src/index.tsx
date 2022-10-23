b95cccca8c3574bb3a9b0309fc0cda2ff3eed015
vintagexav <xavier.de.ryckel@external.mywaypro.be>
2022-10-23 19:02:08 +0200

    cleaner

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
@@ -11,9 +10,9 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/*<Provider store={store}>*/}
       <App />
-    </Provider>
+    {/*</Provider>*/}
   </React.StrictMode>
 );
 
