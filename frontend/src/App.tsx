8c34b21027465d2e5f47f14d6fc1dc37d7c347a0
vintagexav <xavier.de.ryckel@external.mywaypro.be>
2022-10-23 19:09:27 +0200

    eslint & prettier - ready to rock ;)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,9 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <span>
           <span>Learn </span>
         </span>
